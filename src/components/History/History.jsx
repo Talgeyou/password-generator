1418eb51f3f8b02cbbc586927634dc12346c2510
talge <talgeyou@gmail.com>
2022-11-17 15:11:31 +1000

    refactor

--- src/components/History/History.jsx
+++ src/components/History/History.jsx
@@ -1,30 +1,26 @@
 import React from "react";
 import "./History.scss";
+
 function History(props) {
-  //Если пришедший массив больше 5 элементов вырезаем его начало
   if (props.history.length > 5) {
     props.history.shift();
   }
+
   return (
     <div className="history">
       <h2 className="history__title">password History</h2>
       <button
         className="history__button"
         onClick={() => {
-          //Запись истории в локальное хранилище
           localStorage.setItem("history", JSON.stringify([]));
-          //Устанавливаем исторю в соответствии с локальным хранилищем
           props.setHistory(JSON.parse(localStorage.getItem("history")));
         }}
       >
         Clear history
       </button>
       <div className="history__list">
-        {/* мапим для каждого элемента массива текст и иконку */}
-        {props.history.map((historyItem, index) => {
-          // eslint-disable-next-line array-callback-return
-          if (historyItem === "Select length and security") return;
-          return (
+        {props.history.map((historyItem, index) =>
+          historyItem ? (
             <div key={index} className="history__div">
               <p className="history__item">{historyItem}</p>
               <img
@@ -33,8 +29,8 @@ function History(props) {
                 className="history__icon"
               />
             </div>
-          );
-        })}
+          ) : null
+        )}
       </div>
     </div>
   );
